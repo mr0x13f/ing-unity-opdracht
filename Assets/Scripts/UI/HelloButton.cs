e8ebbf3ca2bffa37659d9919cfe7973595432e15
mr0x13f <mr0x13f@gmail.com>
2021-12-04 17:00:08 +0100

    Implemented animal interactions

---
+++ Assets/Scripts/UI/HelloButton.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Zoo
+{
+    [RequireComponent(typeof(Button))]
+    public class HelloButton : MonoBehaviour
+    {
+        [SerializeField]
+        private InputField nameInputField;
+        private Button button;
+
+        void Start()
+        {
+            button = GetComponent<Button>();
+            button.onClick.AddListener(OnClick);
+        }
+
+        void OnClick()
+        {
+            if (nameInputField.text == "")
+                AnimalManager.instance.SayHalloToAll();
+            else
+                AnimalManager.instance.SayHalloByName(nameInputField.text);
+        }
+    }
+}
