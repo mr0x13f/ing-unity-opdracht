96866595daee9587c2ca39720018df6583a28698
mr0x13f <mr0x13f@gmail.com>
2021-12-04 16:20:50 +0100

    Made Debugger a singleton

--- Assets/Scripts/Debugger.cs
+++ Assets/Scripts/Debugger.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,6 +7,24 @@ namespace Zoo
 {
     public class Debugger : MonoBehaviour
     {
+        private static Debugger instance;
+        public static Debugger Instance {
+            get {
+                if (instance != null)
+                    return instance;
+                else
+                    throw new Exception("No instance of Debugger");
+            }
+        }
+
+        private void Awake() {
+            if (instance != null && instance != this) {
+                Destroy(gameObject);
+                return;
+            } else 
+                instance = this;
+        }
+
         public void Log(string message)
         {
             Debug.Log(message);
