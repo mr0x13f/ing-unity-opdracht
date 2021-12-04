1141632322d42dd3927a39a028524f6d5b9b22da
mr0x13f <mr0x13f@gmail.com>
2021-12-04 18:15:03 +0100

    Fixed speechballoons disappearing too early
    Before when a speechballoon was used while it was already active, it would get deactivated after the duration of the first message, thereby cutting the second message off early.
    
    This was fixed by integrating the DeactivateAfterSeconds functionality into the SpeechBalloon component.

--- Assets/Scripts/SpeechBalloon.cs
+++ Assets/Scripts/SpeechBalloon.cs
@@ -9,11 +9,24 @@ namespace Zoo
     {
         [SerializeField]
         private Text text;
+        [SerializeField]
+        private float duration = 5;
 
         public void Say(string message)
         {
             gameObject.SetActive(true);
             text.text = message;
+
+            // Stop the Deactivate coroutine in case it's already running
+            StopCoroutine(nameof(Deactivate));
+            // Disable this gameObject after the duration
+            StartCoroutine(nameof(Deactivate));
+        }
+
+        private IEnumerator Deactivate()
+        {
+            yield return new WaitForSeconds(duration);
+            gameObject.SetActive(false);
         }
 
     }
